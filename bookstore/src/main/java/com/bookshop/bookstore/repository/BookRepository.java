2a6b77ec8b0f360628d874bfccbeb727a504a648
Riya Pramodh <112382228+riyapramodh@users.noreply.github.com>
2023-12-01 00:32:39 +0530

    Update BookRepository.java

--- bookstore/src/main/java/com/bookshop/bookstore/repository/BookRepository.java
+++ bookstore/src/main/java/com/bookshop/bookstore/repository/BookRepository.java
@@ -2,6 +2,21 @@ package com.bookshop.bookstore.repository;
 
 import com.bookshop.bookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface BookRepository extends JpaRepository <Book,Long>{
+import java.util.List;
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByTitleContainingOrAuthorContaining(String title, String author);
+
+    // New method for fetching all book titles
+    @Query("SELECT DISTINCT b.title FROM Book b")
+    List<String> findAllTitles();
+
+    // New method for fetching a book by title
+    Optional<Book> findByTitle(String title);
+
+
 }
