c93827822fe1e7af94965825f169abb806510545
Riya Pramodh <112382228+riyapramodh@users.noreply.github.com>
2023-12-01 00:41:57 +0530

    Update Home.js

--- fullstack-frontend/src/pages/Home.js
+++ fullstack-frontend/src/pages/Home.js
@@ -3,9 +3,8 @@ import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 
 export default function Home() {
+  // const navigate = useNavigate();
   const [books, setBooks] = useState([]);
-
-  const { id } = useParams();
 
   useEffect(() => {
     loadBooks();
@@ -13,12 +12,96 @@ export default function Home() {
 
   const loadBooks = async () => {
     const result = await axios.get("http://localhost:8080/books");
+    
+    result.data.forEach((book) => (book.selected = true));
     setBooks(result.data);
   };
 
   const deleteBook = async (id) => {
+    alert(`Delete book with ID ${id}`);
     await axios.delete(`http://localhost:8080/book/${id}`);
     loadBooks();
+
+    // try{
+    //   await axios.delete(`http://localhost:8080/book/${id}`);
+    //   loadBooks();
+    // }catch (error){
+    //   console.error('Error deleting book:', error);
+    // }
+
+  };
+
+  const printSummary = async () => {
+    const selectedBooks = books.filter((book) => book.selected);
+    const totalCost = selectedBooks.reduce(
+      (total, book) => total + parseFloat(book.cost),
+      0
+    );
+
+    try {
+      const response = await axios.post(
+        "https://us-central1-pivotal-nebula-401108.cloudfunctions.net/savesummary", // Replace with your Cloud Function URL
+        {
+          content: generateSummaryContent(selectedBooks, totalCost),
+        }
+      );
+
+      console.log("Summary saved to Cloud Storage:", response.data);
+      alert("Proceed to Pay");
+    } catch (error) {
+      console.error("Error saving summary:", error);
+      alert("Failed to save summary to Cloud Storage");
+    }
+  };
+
+  const generateSummaryContent = (selectedBooks, totalCost) => {
+    // Generate the summary content here
+    // You can use the same logic as in your printSummary function
+
+    let summaryContent =
+      "<html><head><title>Print Summary</title></head><body>";
+    summaryContent += "<h1>Receipt</h1>";
+    summaryContent +=
+      "<h2>Thank you for purchasing, you may proceed to pay with this receipt</h2>";
+    summaryContent += "<ul>";
+
+    selectedBooks.forEach((book) => {
+      summaryContent += `<li><h5>Title: ${book.title}, Cost: ${book.cost}</h5></li>`;
+    });
+
+    summaryContent += `<li><h4><u>Total Cost: ${totalCost.toFixed(2)}</u></h2></li></ul>`;
+    summaryContent += "</ul></body></html>";
+
+    return summaryContent;
+
+
+
+    // const selectedBooks = books.filter((book) => book.selected);
+    // const totalCost = selectedBooks.reduce((total, book) => total + parseFloat(book.cost), 0);
+
+    // const printWindow = window.open("", "_blank");
+    // printWindow.document.open();
+    // printWindow.document.write(
+    //   "<html><head><title>Print Summary</title></head><body>"
+    // );
+
+    // printWindow.document.write("<h1>Receipt</h1>");
+    // printWindow.document.write("<h2>Thank you for purchasing, you may proceed to pay with this receipt</h2>");
+    // printWindow.document.write("<ul>");
+
+    // selectedBooks.forEach((book) => {
+    //   printWindow.document.write(
+    //     `<li><h5>Title: ${book.title}, Cost: ${book.cost}</h5></li>`
+    //   );
+    // });
+    // printWindow.document.write(`<li><h4><u>Total Cost: ${totalCost.toFixed(2)}</u></h2></li></ul>`);
+
+    // printWindow.document.write("</ul>");
+
+    // printWindow.document.write("</body></html>");
+    // printWindow.document.close();
+    // printWindow.print();
+    // printWindow.close();
   };
 
   return (
@@ -36,10 +119,8 @@ export default function Home() {
           </thead>
           <tbody>
             {books.map((book, index) => (
-              <tr>
-                <th scope="row" key={index}>
-                  {index + 1}
-                </th>
+              <tr key={index}>
+                <th scope="row">{index + 1}</th>
                 <td>{book.title}</td>
                 <td>{book.author}</td>
                 <td>{book.cost}</td>
@@ -68,6 +149,36 @@ export default function Home() {
           </tbody>
         </table>
       </div>
+
+      <div className="d-flex justify-content-center">
+        <button
+          type="button"
+          onClick={printSummary}
+          className="btn btn-primary"
+        >
+          Proceed to Pay
+        </button>
+      </div>
     </div>
   );
-}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
